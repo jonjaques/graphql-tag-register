da030aa59f9c771f4eabbf50fcc81d11afad689a
Jon Jaques <jaquers@gmail.com>
2017-10-21 13:26:36 -0500

    Fix types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,19 @@
 declare module '*.graphql' {
-    import {DocumentNode} from 'graphql';
-    const value: DocumentNode;
-    export = value;
+  import {DocumentNode} from 'graphql';
+  const value: DocumentNode;
+  export = value;
 }
+
+declare module '*.graphqls' {
+  import {DocumentNode} from 'graphql';
+  const value: DocumentNode;
+  export = value;
+}
+
+export interface RegisterOptions {
+  ignoreNodeModules: boolean = true;
+}
+
+declare function register(opts: RegisterOptions): void
+
+export default register
